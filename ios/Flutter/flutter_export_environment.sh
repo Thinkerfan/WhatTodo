8faf6f5395b89e5a1a07741c74fa6d53f8eba028
thinker <731666148@qq.com>
2019-12-04 23:32:07 +0800

    配置Android打包配置

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/cfans/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/cfans/Documents/Flutter-App/WhatTodo"
+export "FLUTTER_TARGET=/Users/cfans/Documents/Flutter-App/WhatTodo/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/cfans/Documents/flutter/bin/cache/artifacts/engine/ios"
